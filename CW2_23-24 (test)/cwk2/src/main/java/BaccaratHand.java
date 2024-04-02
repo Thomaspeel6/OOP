733aef6f8d896dc7f328620f5dbccaddcf5675b7
Thomaspeel6 <146444012+Thomaspeel6@users.noreply.github.com>
2024-04-02 09:35:26 +0000

    esf

--- CW2_23-24 (test)/cwk2/src/main/java/BaccaratHand.java
+++ CW2_23-24 (test)/cwk2/src/main/java/BaccaratHand.java
@@ -5,35 +5,20 @@ import java.util.List;
 
 
 public class BaccaratHand extends CardCollection {
-    
+
     private List<BaccaratCard> baccaratHand;
 
     public BaccaratHand() {
         baccaratHand = new LinkedList<>();
     }
-    
-    
-    public int value() {
-        int sum = 0;
-        for (BaccaratCard baccaratCard: baccaratHand) {
-            sum += baccaratCard.value();
-        }
-        return sum;
-    }
-    
-    public boolean isNatural() {
-        if (value() == 8 || value() == 9 && size() == 2){
-            return true;
-        }
-        return false;
-    }
-    
+
+
 
     public String toString() {
-        String str = "";
+        StringBuilder str = new StringBuilder();
         for (BaccaratCard baccaratCard: baccaratHand) {
-            str += baccaratCard.toString() + " ";
+            str.append(baccaratCard.toString()).append(" ");
         }
-        return str;
+        return str.toString();
     }
 }
